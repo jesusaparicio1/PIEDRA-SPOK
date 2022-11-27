aeea7097343b5607ba73667c0d274886e78b60e3
jesusaparicio1 <jesusaparicio811@gmail.com>
2022-11-27 11:42:43 -0500

    imagenes nuevas

---
+++ js.js
@@ -0,0 +1,12 @@
+let jugador = document.getElementById('jugador1');
+let jugador2 = document.getElementById('jugador2');
+let versus = document.getElementById('versus');
+
+let piedra,papel,tijeras,lagarto,spoke;
+let imagenVersus;
+
+imagenVersus = new Image();
+imagenVersus.src = 'versus.png';
+
+
+
